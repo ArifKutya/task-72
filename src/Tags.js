947dbc587789479f1d9cbe1d010359b7e8a84cbe
Arif <arifkutq63@gmail.com>
2023-01-12 17:17:09 +0200

    work

---
+++ src/Tags.js
@@ -0,0 +1,22 @@
+function Tags() {
+  return (
+    <div className="tags-input-container">
+      <div className="tag-item">
+        <span className="#"></span>
+        <span className="text">boomdotdev</span>
+        <span className="close">&times;</span>
+      </div> <div className="tag-item">
+      <span className="text">task</span>
+      <span className="close">&times;</span>
+    </div> <div className="tag-item">
+      <span className="text">tags</span>
+      <span className="close">&times;</span>
+    </div> <div className="tag-item">
+      <span className="text">react</span>
+      <span className="close">&times;</span>
+    </div>
+    </div>
+  );
+}
+
+export default Tags;
